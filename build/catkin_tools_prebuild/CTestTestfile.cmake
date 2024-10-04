bc2902b6ccbd0e73d95ad4f3d08a4af79678649d
Srijan Pal <pal00036@umn.edu>
2024-10-04 03:43:04 -0500

    Delete build directory

--- build/catkin_tools_prebuild/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/build/catkin_tools_prebuild
-# Build directory: /home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/build/catkin_tools_prebuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
