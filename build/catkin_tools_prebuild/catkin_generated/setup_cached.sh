b48bc6a2ab9da8e373ec9854ed38621438669c3d
srijanpal07 <123281965+srijanpal07@users.noreply.github.com>
2024-10-04 01:00:52 -0500

    package update

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/srijan1804/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files/WindowsApps/CanonicalGroupLimited.Ubuntu18.04LTS_1804.6.4.0_x64__79rhkp1fndgsc:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v12.2/bin:/mnt/c/Program Files/NVIDIA Corporation/NVIDIA NvDLISR:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files/Microsoft SQL Server/Client SDK/ODBC/170/Tools/Binn/:/mnt/c/Program Files (x86)/Microsoft SQL Server/150/Tools/Binn/:/mnt/c/Program Files/Microsoft SQL Server/150/Tools/Binn/:/mnt/c/Program Files/Microsoft SQL Server/150/DTS/Binn/:/mnt/c/Program Files (x86)/Windows Kits/8.1/Windows Performance Toolkit/:/mnt/c/Program Files/Microsoft SQL Server/130/Tools/Binn/:/mnt/c/Users/Srijan/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/Srijan/.dotnet/tools:/mnt/c/Users/Srijan/AppData/Local/GitHubDesktop/bin:/mnt/c/Users/Srijan/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/Srijan/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Windows PowerShell:/mnt/c/Users/Srijan/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/Scripts:/snap/bin'
+export PWD='/home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/build/catkin_tools_prebuild'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/srijan1804/github/Autonomous-Drone-for-Dynamic-Smoke-Plume-Tracking/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
